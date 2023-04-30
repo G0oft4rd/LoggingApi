94648832db44859dbb22b8b19d214149abf5b804
unknown <refael12212@gmail.com>
2023-05-01 00:01:00 +0300

    added javadoc

--- src/main/java/org/goof/LoggingApi.java
+++ src/main/java/org/goof/LoggingApi.java
@@ -3,9 +3,17 @@ package org.goof;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * A wrapper class for log4j2's LogManager that adds guards for async loggers
+ */
 public class LoggingApi {
     private static boolean addedGuards = false;
 
+    /**
+     * A method for getting a logger for a specific class, returns the root logger if not logger is defined for that class
+     * @param callingClass The class of the logger
+     * @return A logger of that class or root logger if not defined
+     */
     public static Logger getLogger(Class callingClass) {
         if (!addedGuards) {
             addedGuards = true;
